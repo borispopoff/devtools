de4b0b40d9a7530378c772a3390a67eeb6dfd565
borispopoff <58560689+borispopoff@users.noreply.github.com>
2024-08-25 16:49:22 +0200

    update binary dependencies according to doc

--- emacs-bootstrap.sh
+++ emacs-bootstrap.sh
@@ -8,11 +8,11 @@
 ## PREREQUISITES
 # list of prerequisite libraries and tools
 # required for compilation;
-pacman -Sy --needed \
-  base-devel \
-  autoconf \
+pacman -S --needed base-devel \
   mingw-w64-x86_64-toolchain \
   mingw-w64-x86_64-xpm-nox \
+  mingw-w64-x86_64-gmp \
+  mingw-w64-x86_64-gnutls \
   mingw-w64-x86_64-libtiff \
   mingw-w64-x86_64-giflib \
   mingw-w64-x86_64-libpng \
@@ -20,15 +20,20 @@ pacman -Sy --needed \
   mingw-w64-x86_64-librsvg \
   mingw-w64-x86_64-libwebp \
   mingw-w64-x86_64-lcms2 \
-  mingw-w64-x86_64-jansson \
   mingw-w64-x86_64-libxml2 \
-  mingw-w64-x86_64-gnutls \
   mingw-w64-x86_64-zlib \
-  mingw-w64-x86_64-harfbuzz
+  mingw-w64-x86_64-harfbuzz \
+  mingw-w64-x86_64-libgccjit \
+  mingw-w64-x86_64-sqlite3 \
+  mingw-w64-x86_64-tree-sitter
 
 # there is no git in the default config.
 # need to install it
 pacman -Sy git
+
+# there is no autoconf in the default config.
+# need to install it
+pacman -Sy --needed autoconf
 
 
 ################
